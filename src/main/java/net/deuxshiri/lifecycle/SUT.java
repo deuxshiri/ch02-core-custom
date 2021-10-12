71475601b4259f1925e01af31d171babc17bde41
Deuxshiri <deuxshiri@gmail.com>
2021-10-12 14:59:00 +0330

    Initial Commit.

---
+++ src/main/java/net/deuxshiri/lifecycle/SUT.java
@@ -0,0 +1,25 @@
+package net.deuxshiri.lifecycle;
+
+public class SUT {
+
+    private final String systemName;
+
+    public SUT(String systemName) {
+        this.systemName = systemName;
+        System.out.println(systemName + " from class " + getClass().getSimpleName() + " is initializing.");
+    }
+
+    public boolean canReceiveRegularWork() {
+        System.out.println(systemName + "from class " + getClass().getSimpleName() + " can receive regular work.");
+        return true;
+    }
+
+    public boolean canReceiveAdditionalWork() {
+        System.out.println(systemName + " from class " + getClass().getSimpleName() + " cannot receive additional work.");
+        return false;
+    }
+
+    public void close() {
+        System.out.println(systemName + " from class " + getClass().getSimpleName() + " is closing.");
+    }
+}
